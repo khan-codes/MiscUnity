b4f1e66a69c846ac33c3c80dff433745a5ea75d8
Dawood Sher Jan <alpha72784@outlook.com>
2021-05-30 13:16:14 +0500

    Some misc ai scripts used in different projects. Keeping them here for future reference.

---
+++ AI/CrowdControlAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour {
+
+	GameObject[] goalLocations;
+	UnityEngine.AI.NavMeshAgent agent;
+
+
+	// Use this for initialization
+	void Start () {
+		goalLocations = GameObject.FindGameObjectsWithTag("goal");
+		agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+		agent.SetDestination(goalLocations[Random.Range(0,goalLocations.Length)].transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
